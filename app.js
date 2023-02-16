3e0f349e5cc3261990129b68d378bdac842e5bc0
SerhiySk <124919437+SerhiySk@users.noreply.github.com>
2023-02-16 15:50:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -18,12 +18,19 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+  })
+);
 
 app.use(helmet());
 app.use(cors());
-app.use(xss);
-app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
-
+app.use(xss());
 // extra packages
 
 // routes
