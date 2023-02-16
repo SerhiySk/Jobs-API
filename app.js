4b34be953bf0773ea23a73c2a69aea2738262422
SerhiySk <124919437+SerhiySk@users.noreply.github.com>
2023-02-16 22:04:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
 require('express-async-errors');
+
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
+
+//Swagger
+
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const express = require('express');
 const app = express();
@@ -32,7 +39,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 // extra packages
-
+app.get('/', (req, res) => {
+  res.send('<h1>jobs api</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
@@ -54,3 +64,4 @@ const start = async () => {
 };
 
 start();
+
